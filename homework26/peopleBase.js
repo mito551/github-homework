4929ce2b3b0727620b0ca491a08e6dec06009fe3
mitori itoshiki <mito551@gmail.com>
2021-08-27 11:58:46 +0200

    adding homework for building an input field with multiple data fields

---
+++ homework26/peopleBase.js
@@ -0,0 +1,55 @@
+const nameInput = document.querySelector('#formName')
+const birthdayInput = document.querySelector('#formBirth')
+const countryInput = document.querySelector('#formCountry')
+const pizzaInput = document.querySelector('#formPizza')
+const btn = document.querySelector('#submitForm')
+const list = document.querySelector('#app')
+
+const peopleList = [];
+
+let lastId = 0;
+
+const renderTodo = () => {
+    list.innerHTML = "";
+    peopleList.forEach((person) => {
+        const personContainer = document.createElement('section');
+        personContainer.classList.add("item")
+        const personName = document.createElement('span');
+        personName.innerText = "Name: " + person.name;
+        const personAge = document.createElement('span');
+        personAge.innerText = "Age: " + person.age;
+        const personCountry = document.createElement('span');
+        personCountry.innerText = "Living in: " + person.country;
+        const personPizza = document.createElement('span');
+        personPizza.innerText = person.isPsychopath ? "Personality: Normal Person" : "Personality: Obnoxious";
+        personContainer.appendChild(personName);
+        personContainer.appendChild(personAge);
+        personContainer.appendChild(personCountry);
+        personContainer.appendChild(personPizza);
+        list.appendChild(personContainer);
+
+    })
+}
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const newName = nameInput.value.trim();
+    const newBirthday = new Date(birthdayInput.value);
+    const newCountry = countryInput.value;
+    const newPizza = pizzaInput.checked;
+    if (newName === "") return;
+    // if (newBirthday == NaN) return;
+    const newPerson = {
+        id: ++lastId,
+        name: newName,
+        age: 2021 - newBirthday.getUTCFullYear(),
+        country: newCountry,
+        isPsychopath: newPizza
+    };
+    peopleList.push(newPerson);
+    console.log(peopleList)
+    renderTodo();
+    nameInput.value = "";
+})
+
+//text-decoration-line-through
