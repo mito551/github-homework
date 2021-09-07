b1fc9987702df0982d325462e7489e33542c3d6a
mitori itoshiki <mito551@gmail.com>
2021-09-07 21:51:32 +0200

    added an edit button

--- homework30/peopleBase.js
+++ homework30/peopleBase.js
@@ -4,6 +4,7 @@ const countryInput = document.querySelector('#formCountry')
 const pizzaInput = document.querySelector('#formPizza')
 const btn = document.querySelector('#submitForm')
 const list = document.querySelector('#app')
+let lastId = 0;
 
 class Person {
     constructor(nextID, newName, newAge, newCountry, newPizza) {
@@ -25,18 +26,97 @@ const peopleList = [{
     isDeleted: false
 }];
 
+function renderForm(container, person, editingObject){
+    const confirmButton = document.createElement('div');
+    confirmButton.classList.add('confirmButton');
+    confirmButton.innerText = "✓"
+    confirmButton.id = `id_${person.id}`
 
-let lastId = 0;
+    const editingContainer = document.createElement('section');
+    editingContainer.classList.add("item")
+
+    const personNameLabel = document.createElement('label');
+    personNameLabel.innerText = "New Name:";
+    const personName = document.createElement('input');
+    personName.setAttribute("type", "text")
+    personName.setAttribute("value", editingObject.name)
+
+    const personAgeLabel = document.createElement('label');
+    personAgeLabel.innerText = "New Birthday:";
+    const personAge = document.createElement('input');
+    personAge.setAttribute("type", "date")
+
+    const personCountryLabel = document.createElement('label');
+    personCountryLabel.innerText = "New Country:";
+    const personCountry = document.createElement('select');
+    const personCountryGermany = document.createElement('option');
+    personCountryGermany.setAttribute("value", "Germany")
+    personCountryGermany.innerText = "Germany"
+    const personCountryOther = document.createElement('option');
+    personCountryOther.setAttribute("value", "Other")
+    personCountryOther.innerText = "Other"
+    personCountry.appendChild(personCountryGermany)
+    personCountry.appendChild(personCountryOther)
+
+    const personPizzaLabel = document.createElement('label');
+    personPizzaLabel.innerText = "New Pizza Preference:";
+    const personPizza = document.createElement('input');
+    personPizza.setAttribute("type", "checkbox")
+    personPizza.checked = person.isPsychopath
+
+    container.appendChild(confirmButton);
+    editingContainer.appendChild(personNameLabel);
+    editingContainer.appendChild(personName);
+    editingContainer.appendChild(personAgeLabel);
+    editingContainer.appendChild(personAge);
+    editingContainer.appendChild(personCountryLabel);
+    editingContainer.appendChild(personCountry);
+    editingContainer.appendChild(personPizzaLabel);
+    editingContainer.appendChild(personPizza);
+    container.appendChild(editingContainer);
+    confirmButtonActivate(confirmButton, editingContainer, editingObject, personName, personAge, personCountry, personPizza);
+}
+
+const deleteButtonActivate = (deleteBtn) => {
+    deleteBtn.addEventListener('click', event => {
+        const id = +event.target.id.split("_")[1]
+        const deletion = peopleList.find(param => param.id === id)
+        deletion.isDeleted = true;
+        renderTodo();
+    })
+}
+
+const editButtonActivate = (editBtn, personContainer, container, person) => {
+    editBtn.addEventListener('click', event => {
+        const id = +event.target.id.split("_")[1]
+        const editingObject = peopleList.find(param => param.id === id)
+        personContainer.classList.add('deleted')
+        editBtn.classList.add('deleted')
+
+        renderForm(container, person, editingObject)
+    })
+}
+
+const confirmButtonActivate = (confirmBtn, editingContainer, editingObject, personName, personAge, personCountry, personPizza) => {
+    confirmBtn.addEventListener('click', event => {
+        const newName = personName.value.trim();
+        const newBirthday = new Date(personAge.value);
+        const newCountry = personCountry.value;
+        const newPizza = personPizza.checked;
+        if (newName === "") return;
+        if (newBirthday.value === "") return;
+        editingContainer.classList.remove('deleted')
+        editingObject.name = newName
+        editingObject.age = 2021 - newBirthday.getUTCFullYear()
+        editingObject.country = newCountry
+        editingObject.isPsychopath = newPizza
+        renderTodo();
+    })
+}
 
 const renderTodo = () => {
     list.innerHTML = "";
     peopleList.forEach((person) => {
-        const deleteButton = document.createElement('div');
-        deleteButton.classList.add('deleteButton');
-        deleteButton.innerText = "D"
-        const editButton = document.createElement('div');
-        editButton.classList.add('editButton');
-        editButton.innerText = "E"
 
         const container = document.createElement('div');
         container.classList.add('wrapItUp')
@@ -50,7 +130,14 @@ const renderTodo = () => {
         personCountry.innerText = "Living in: " + person.country;
         const personPizza = document.createElement('span');
         personPizza.innerText = person.isPsychopath ? "Personality: Normal Person" : "Personality: Obnoxious";
+        const deleteButton = document.createElement('div');
+        deleteButton.classList.add('deleteButton');
+        deleteButton.innerText = "D"
         deleteButton.id = `id_${person.id}`
+        const editButton = document.createElement('div');
+        editButton.classList.add('editButton');
+        editButton.innerText = "E"
+        editButton.id = `id_${person.id}`
 
         if (person.isDeleted) {
             container.classList.add('deleted')
@@ -64,6 +151,9 @@ const renderTodo = () => {
         personContainer.appendChild(personPizza);
         container.appendChild(personContainer);
         list.appendChild(container);
+
+        deleteButtonActivate(deleteButton);
+        editButtonActivate(editButton, personContainer, container, person);
 
     })
 }
@@ -85,12 +175,6 @@ btn.addEventListener('click', (event) => {
     nameInput.value = "";
 })
 
-list.addEventListener('click', event => {
-    const id = +event.target.id.split("_")[1]
-    const deletion = peopleList.find(param => param.id === id)
-    deletion.isDeleted = true;
-    renderTodo();
-    // event.target.classList.toggle(`text-decoration-line-through`)
-})
+
 
 //text-decoration-line-through
